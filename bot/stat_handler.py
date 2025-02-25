27251fd71e2a130d4c7d18ebbb259b790e8af59f
dnaumov <dnaumov09@gmail.com>
2025-02-25 17:06:03 +0300

    more stat

--- bot/stat_handler.py
+++ bot/stat_handler.py
@@ -51,44 +51,50 @@ async def cmd_pipeline_detailed(message: Message):
 
     for i in range(len(month_stat)):
         result = f"<b>{month_stat[i]['title']} ({month_stat[i]['vendor_code']})</b>"
-        result += "\n\n" + build_daily_stat('Сегодня', now, today_stat[i])
-        result += "\n\n" + build_daily_stat('Вчера', now - timedelta(days=1), yesterday_stat[i])
-        result += "\n\n" + build_weekly_stat(now, week_stat[i])
-        result += "\n\n" + build_monthly_stat(now, month_stat[i])    
+        result += "\n\n" + build_daily_stat('Сегодня', now, today_stat[i], True)
+        result += "\n\n" + build_daily_stat('Вчера', now - timedelta(days=1), yesterday_stat[i], True)
+        result += "\n\n" + build_weekly_stat(now, week_stat[i], True)
+        result += "\n\n" + build_monthly_stat(now, month_stat[i], True)    
         await message.answer(result)
 
 
-def build_daily_stat(when, day, stat) -> str:
+def build_daily_stat(when, day, stat, is_detailed: bool = False) -> str:
     day_str = day.strftime("%d.%m - %A")
     date_part, weekday_name = day_str.split(" - ")
 
     period_details = f"{weekday_name.capitalize()}, {date_part}"
     result = f"<b>{when} ({period_details}):</b>\n"
-    result += build_stat_data(stat)
+    result += build_stat_data(stat, is_detailed)
     return result
 
 
-def build_weekly_stat(day, stat) -> str:
+def build_weekly_stat(day, stat, is_detailed: bool = False) -> str:
     weekday = day.weekday()
     monday = day - timedelta(days=weekday)
     sunday = monday + timedelta(days=6)
 
-    result = f"<b>Неделя #{day.isocalendar()[1]} (, {monday.strftime('%d.%m')} - {sunday.strftime('%d.%m')}):</b>\n"
-    result += build_stat_data(stat)
+    result = f"<b>Неделя #{day.isocalendar()[1]} ({monday.strftime('%d.%m')} - {sunday.strftime('%d.%m')}):</b>\n"
+    result += build_stat_data(stat, is_detailed)
     return result
 
 
-def build_monthly_stat(day, stat) -> str:
+def build_monthly_stat(day, stat, is_detailed: bool = False) -> str:
     result = f"<b>{months_nominative[day.month]}, {day.year}:</b>\n"
-    result += build_stat_data(stat)
+    result += build_stat_data(stat, is_detailed)
     return result
 
 
-def build_stat_data(stat) -> str:
+def build_stat_data(stat, is_detailed: bool = False) -> str:
     oreders_count = stat['orders_count']
     buyouts_count = stat['buyouts_count']
     cancel_count = stat['cancel_count']
-    result = f"Заказов: <b>{oreders_count}</b>"
+
+    result = ""
+    if is_detailed:
+        result += f"Открытий карточки: <b>{stat['open_card_count']}</b>\n"
+        result += f"Добавлений в корзину: <b>{stat['add_to_cart_count']}</b>\n"
+
+    result += f"Заказов: <b>{oreders_count}</b>"
     result += f" ({format(round(stat['orders_sum_rub']), ",d").replace(",", " ")} руб.)\n" if oreders_count > 0 else "\n"
     result += f"Выкупов: <b>{buyouts_count}</b>"
     result += f" ({format(round(stat['buyouts_sum_rub']), ",d").replace(",", " ")} руб.)\n" if buyouts_count > 0 else "\n"
