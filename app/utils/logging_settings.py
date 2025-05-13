900411444ec1e094c05ba8f460b12bd21c2660b0
dnaumov <dnaumov09@gmail.com>
2025-05-13 12:08:04 +0300

    colored logs

--- app/utils/logging_settings.py
+++ app/utils/logging_settings.py
@@ -1,29 +1,44 @@
 import logging
-from colorlog import ColoredFormatter
+from colorama import Fore, Style, init
 
-TEXT_FORMAT = "%(log_color)s[%(levelname)s] %(asctime)s: %(message)s"
-DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-LOG_COLORS = {
-    # 'DEBUG':    'cyan',
-    # 'INFO':     'green',
-    'WARNING':  'yellow',
-    'ERROR':    'red',
-    'CRITICAL': 'bold_red',
+LEVEL_COLORS = {
+        logging.DEBUG: Fore.CYAN,
+        logging.INFO: Fore.WHITE,
+        logging.WARNING: Fore.YELLOW,
+        logging.ERROR: Fore.RED,
+        logging.CRITICAL: Fore.RED + Style.BRIGHT
     }
 
-formatter = ColoredFormatter(
-    fmt=TEXT_FORMAT,
-    datefmt=DATE_FORMAT,
-    log_colors=LOG_COLORS
-)
+MESSAGE_FORMAT = '[%(levelname)s] %(asctime)-30s:  %(message)s'
+DATE_FORMAT = '%d-%m-%Y %H:%M:%S'
+
+class Formatter(logging.Formatter):
+    def format(self, record):
+        timestamp = self.formatTime(record, self.datefmt)
+        color = self.LEVEL_COLORS.get(record.levelno, "")
+        levelname = f"[{record.levelname}]"
+        return f"{color}{levelname:<10} {timestamp}:    {record.getMessage()}{Style.RESET_ALL}"
+
+
+def set_loggers_level(level):
+    logging.getLogger(__name__).setLevel(level)
+    
+    # for not main loggers
+    if level < logging.WARNING:
+        level = logging.WARNING    
+    logging.getLogger('httpx').setLevel(level)
+    logging.getLogger('telegram').setLevel(level)
+
 
 def init_logging(level: int = logging.INFO):
-    logger = logging.getLogger()
-    logger.setLevel(level)
+    init(autoreset=True)
+    set_loggers_level(level)
 
     handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
+    handler.setFormatter(Formatter(fmt=MESSAGE_FORMAT,datefmt='%d-%m-%Y %H:%M:%S'))
+    
+    logger = logging.getLogger()
     logger.addHandler(handler)
 
-    logging.getLogger('httpx').setLevel(level=logging.WARNING if level < logging.WARNING else level)
-    logging.getLogger('telegram').setLevel(level=logging.WARNING if level < logging.WARNING else level)
+
+    
