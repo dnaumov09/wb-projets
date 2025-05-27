86438bd8d30123c32f16b0a8a04baeb709c078f2
dnaumov <dnaumov09@gmail.com>
2025-05-27 20:44:45 +0300

    web

---
+++ app/web/templates/static/tailwind.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ['Inter', 'sans-serif']
+            },
+            colors: {
+                metabaseBlue: '#509EE3',
+                metabaseBlueHover: '#3B8DD3',
+                metabaseTeal: '#6FD1B6',
+                metabaseDark: '#2D2D2D',
+                metabaseGray: '#F5F6F7',
+                metabaseRed: '#E36262'
+            }
+        }
+    }
+}
